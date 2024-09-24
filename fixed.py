5fc1a3adb4c3d4ccd0d071e037b97c988fd7d9bc
Mesozoic Egg <mesozoic.egg@proton.me>
2024-09-24 17:38:32 +0800

    error checking

--- fixed.py
+++ fixed.py
@@ -13,34 +13,44 @@ $ python fixed.py
 
 # Below is pasted from the ops_metal.py file from metal-cdll branch from https://github.com/tinygrad/tinygrad/pull/6545
 from __future__ import annotations
-import  ctypes, functools
+import ctypes
+import functools
 from typing import Any, cast, TypeVar
 from ctypes.macholib.dyld import dyld_find
 
-class objc_id(ctypes.c_void_p): # This prevents ctypes from converting response to plain int, and dict.fromkeys() can use it to dedup
-  def __hash__(self): return hash(self.value)
-  def __eq__(self, other): return self.value == other.value
 
-class objc_instance(objc_id): # method with name "new", "alloc" should be freed after use
-  def __del__(self): msg(self, "release")
+# This prevents ctypes from converting response to plain int, and dict.fromkeys() can use it to dedup
+class objc_id(ctypes.c_void_p):
+    def __hash__(self): return hash(self.value)
+    def __eq__(self, other): return self.value == other.value
+
+
+class objc_instance(objc_id):  # method with name "new", "alloc" should be freed after use
+    def __del__(self): msg(self, "release")
+
 
 @functools.lru_cache(None)
 def sel(name: str): return libobjc.sel_registerName(name.encode())
 
+
 def find_and_load(name: str): return ctypes.CDLL(dyld_find(name))
 
+
 class MTLResourceOptions:
-  MTLResourceCPUCacheModeDefaultCache = 0
-  MTLResourceStorageModeShared = 0 << 4
+    MTLResourceCPUCacheModeDefaultCache = 0
+    MTLResourceStorageModeShared = 0 << 4
+
 
 class MTLPipelineOption:
-  MTLPipelineOptionNone = 0
+    MTLPipelineOptionNone = 0
+
 
 libobjc = find_and_load("/usr/lib/libobjc.dylib")
 libmetal = find_and_load("/Library/Frameworks/Metal.framework/Metal")
 # Must be loaded for default Metal Device: https://developer.apple.com/documentation/metal/1433401-mtlcreatesystemdefaultdevice?language=objc
 find_and_load("/Library/Frameworks/CoreGraphics.framework/CoreGraphics")
-libdispatch = find_and_load("/usr/lib/libSystem.dylib") # libdispatch is part of libSystem on mac
+# libdispatch is part of libSystem on mac
+libdispatch = find_and_load("/usr/lib/libSystem.dylib")
 libobjc.objc_getClass.restype = objc_id
 libobjc.sel_registerName.restype = objc_id
 libmetal.MTLCreateSystemDefaultDevice.restype = objc_instance
@@ -48,16 +58,22 @@ libdispatch.dispatch_data_create.restype = objc_instance
 
 T = TypeVar("T")
 # Ignore mypy error reporting incompatible default, because typevar default only works on python 3.12
-def msg(ptr: objc_id, selector: str, /, *args: Any, restype: type[T] = objc_id) -> T: # type: ignore [assignment]
-  sender = libobjc["objc_msgSend"] # Using attribute access returns a new reference so setting restype is safe
-  sender.restype = restype
-  return sender(ptr, sel(selector), *args)
 
-def to_ns_str(s: str): return msg(libobjc.objc_getClass(b"NSString"), "stringWithUTF8String:", s.encode(), restype=objc_instance)
+
+# type: ignore [assignment]
+def msg(ptr: objc_id, selector: str, /, *args: Any, restype: type[T] = objc_id) -> T:
+    # Using attribute access returns a new reference so setting restype is safe
+    sender = libobjc["objc_msgSend"]
+    sender.restype = restype
+    return sender(ptr, sel(selector), *args)
+
+
+def to_ns_str(s: str): return msg(libobjc.objc_getClass(b"NSString"),
+                                  "stringWithUTF8String:", s.encode(), restype=objc_instance)
 
 
 # Here's the fixed example by calling the dyld runtime directly
-src="""#include <metal_stdlib>
+src = """#include <metal_stdlib>
 using namespace metal;
 kernel void r_5(device int* data0, const device int* data1, uint3 gid [[threadgroup_position_in_grid]], uint3 lid [[thread_position_in_threadgroup]]) {
   int acc0 = -2147483648;
@@ -79,22 +95,56 @@ kernel void r_5(device int* data0, const device int* data1, uint3 gid [[threadgr
   *(data0+0) = alu9;
 }"""
 
-options = msg(libobjc.objc_getClass(b"MTLCompileOptions"), "new", restype=objc_instance)
+
+def get_err_str(err):
+    if err.value is None:
+        print("Error is None")
+        return
+    return bytes(msg(msg(err, "localizedDescription", restype=objc_instance),
+                     "UTF8String", restype=ctypes.c_char_p)).decode()
+
+
+options = msg(libobjc.objc_getClass(b"MTLCompileOptions"),
+              "new", restype=objc_instance)
 
 device = libmetal.MTLCreateSystemDefaultDevice()
 
 compileError = objc_instance(0)
 library = msg(device, "newLibraryWithSource:options:error:", to_ns_str(src),
               options, ctypes.byref(compileError), restype=objc_instance)
+if library.value is None:
+    print("library compile failed")
+    print("reason:", get_err_str(compileError))
+    exit(1)
+
 library_contents = msg(library, "libraryDataContents", restype=objc_instance)
-lib_bytes = ctypes.string_at(msg(library_contents, "bytes"), cast(int, msg(library_contents, "length", restype=ctypes.c_ulong)))
+lib_bytes = ctypes.string_at(msg(library_contents, "bytes"), cast(
+    int, msg(library_contents, "length", restype=ctypes.c_ulong)))
 
 name = "r_5"
 data = libdispatch.dispatch_data_create(lib_bytes, len(lib_bytes), None, None)
-library2 = msg(device, "newLibraryWithData:error:", data, None, restype=objc_instance)
-fxn = msg(library, "newFunctionWithName:", to_ns_str(name), restype=objc_instance)
-descriptor = msg(libobjc.objc_getClass(b"MTLComputePipelineDescriptor"), "new", restype=objc_instance)
+
+newLibError = objc_instance(0)
+library2 = msg(device, "newLibraryWithData:error:", data,
+               ctypes.byref(newLibError), restype=objc_instance)
+
+if library2.value is None:
+    print("Library creation failed")
+    print("reason:", get_err_str(newLibError))
+    exit(1)
+
+fxn = msg(library2, "newFunctionWithName:",
+          to_ns_str(name), restype=objc_instance)
+descriptor = msg(libobjc.objc_getClass(
+    b"MTLComputePipelineDescriptor"), "new", restype=objc_instance)
+
 msg(descriptor, "setComputeFunction:", fxn)
 msg(descriptor, "setSupportIndirectCommandBuffers:", True)
+
+newComputePipelineError = objc_instance(0)
 pipeline_state = msg(device, "newComputePipelineStateWithDescriptor:options:reflection:error:",
-                          descriptor, MTLPipelineOption.MTLPipelineOptionNone, None, None, restype=objc_instance)
+                     descriptor, MTLPipelineOption.MTLPipelineOptionNone, None, ctypes.byref(newComputePipelineError), restype=objc_instance)
+if pipeline_state.value is None:
+    print("pipeline creation failed")
+    print("reason:", get_err_str(newComputePipelineError))
+    exit(1)
